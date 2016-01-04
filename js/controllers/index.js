8c42a1df5c2fb602a3d3f56c08e44b24a9ca3213
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-04 16:29:10 +0100

    Le controlleur récupère désormais les données live via SteamDB

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,4 +1,12 @@
-app.controller('indexCtrl',function($scope,$http,$stateParams,$location)
+app.controller('indexCtrl',function($scope,$http)
 {
+    $http.get(steamDbApiIndex+"/753w.json")   //appel api steam
+    .success(function(r)
+    {
+        console.log(r.players);
+        $scope.current = r.players[r.players.length-1];
+        $scope.last = r.players[r.players.length-2];
+        console.log($scope.last);
+    });
     //Ici on récupère le nombre d'utilisateurs en ligne en live et on simule un changement en temps réèl (alors qu'en fait non mais les gens sont con ils verront rien mdr)
 });
