688bd03382a2da5a5140ca997c1b703a51e26582
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-04 16:27:34 +0100

    Suppression de modules useless

--- js/app.js
+++ js/app.js
@@ -1,26 +1,11 @@
+var steamDbApiIndex = "https://ravenholm.steamdb.info";
+
 var app = angular.module('dataviz', [
-'ui.router'
-    ]);
 
-app.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
-  
-  $urlRouterProvider.otherwise("/");
-  //
-  // Now set up the states
-  $stateProvider
-    .state('user', {
-      url: "/user/:tvId",
-      templateUrl: "views/user.html",
-      controller: "userCtrl"
-    })
-    .state('tvEpisode', {
-      url: "/tv/:tvId/season/{seasonNumber:int}/episode/{episodeNumber:int}",
-      templateUrl: "views/tv_episode.html",
-      controller: "tvEpisodeCtrl"
-    })
-});
 
-app.controller('mainCtrl', ['$scope', '$http','$rootScope', function($scope, $http,$rootScope)
+]);
+
+app.controller('mainCtrl', ['$scope', function($scope)
 {
 	// controlleur principal
 	// ici se trouvent les fnction et variables globales
