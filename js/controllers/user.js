df466bacb0eb6baf8df762f625df102db2c33112
Thibaut <thibaut.arnoud@gmail.com>
2016-01-05 16:55:10 +0100

    Update controller user.js

--- js/controllers/user.js
+++ js/controllers/user.js
@@ -3,7 +3,16 @@ app.controller('userCtrl',function($scope,$http)
     $http.get(api.steam+"/ISteamUser/GetPlayerSummaries/v0002/?key="+api.key+"&steamids=76561197960435530")   //appel api steam
     .success(function(r)
     {
-    	console.log(r);
-    	console.log('test');
+        $scope.user = r.response.players[0];
+        retreiveFriendsList($scope.user.steamid);
     });
+
+    function retreiveFriendsList(userId)
+    {
+        $http.get(api.steam+"/ISteamUser/GetFriendList/v0001/?key="+api.key+"&steamid="+userId+"&relationship=friend")
+            .success(function(t)
+            {
+                $scope.userFriendsList = t.friendslist.friends;
+            });
+    }
 });
