babca5b02565209a451cea50e5be59b6fdf8c836
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-05 14:48:14 +0100

    Base du controlleur game

--- js/controllers/game.js
+++ js/controllers/game.js
@@ -1,21 +1,13 @@
 app.controller('gameCtrl',function($scope,$http)
 {
+    $scope.game = {};
     $scope.setGame = function(appid)
     {
-        $http.get(api.steamDB+"/753w.json")   //appel api steam
+        $http.get(api.steamDB+"/"+appid+"w.json")   //appel api steam
         .success(function(r)
         {
-            $scope.current = r.players[r.players.length-1];
-            $scope.last = r.players[r.players.length-2];
+            $scope.game = r;
         });
     }
-    
-
-    $http.get(api.steamSpy+"?request=top100in2weeks")
-    .success(function(r)
-    {
-    	$scope.top100 = _.sortBy(r, 'players_2weeks').reverse();
-    	console.log($scope.top100);
-    });
     //Ici on récupère le nombre d'utilisateurs en ligne en live et on simule un changement en temps réèl (alors qu'en fait non mais les gens sont con ils verront rien mdr)
 });
