ace1cd836e8bc4ee30e3aa7193d5f0a5a73870e6
Hema.Lambert <lamberthema@gmail.com>
2016-01-05 15:55:14 +0100

    test graph

--- index.php
+++ index.php
@@ -18,6 +18,9 @@
 	<!-- loading all prerequisite components -->
 	<script src="js/vendor/jquery.min.js"></script>
 	<script src="js/vendor/angular.min.js"></script>
+	<script src="js/vendor/underscore.min.js"></script>
+	<script src="https://code.highcharts.com/highcharts.js"></script>
+	<script src="https://code.highcharts.com/modules/exporting.js"></script>
 
 	<script>
 	<?php
@@ -39,6 +42,9 @@
 </head>
 <body ng-controller="mainCtrl">
 	<div ui-view></div>
+	<div ng-controller="indexCtrl" ng-init="graph()">
+		<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+	</div>
 	<div ng-include="'views/sidebar.html'">
     </div>
 </body>
--- js/controllers/index.js
+++ js/controllers/index.js
@@ -31,4 +31,102 @@ app.controller('indexCtrl',function($scope,$http)
     	console.log($scope.topGames);
     });
     //Ici on récupère le nombre d'utilisateurs en ligne en live et on simule un changement en temps réèl (alors qu'en fait non mais les gens sont con ils verront rien mdr)
+    
+    $scope.graph = function () {
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+
+        // Load the fonts
+        Highcharts.createElement('link', {
+           href: '//fonts.googleapis.com/css?family=Unica+One',
+           rel: 'stylesheet',
+           type: 'text/css'
+        }, null, document.getElementsByTagName('head')[0]);
+
+        $('#container').highcharts({
+            chart: {
+                backgroundColor: {
+                 linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+                 stops: [
+                    [0, '#2a2a2b'],
+                    [1, '#3e3e40']
+                 ]
+              },
+              style: {
+                 fontFamily: "'Unica One', sans-serif"
+              },
+              plotBorderColor: '#606063',
+                type: 'areaspline',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginRight: 10,
+                events: {
+                    load: function () {
+
+                        // set up the updating of the chart each second
+                        var series = this.series[0];
+                        setInterval(function () {
+                            var x = (new Date()).getTime(), // current time
+                                y = $scope.current
+                            series.addPoint([x, y], true, true);
+                        }, 1000);
+                    }
+                }
+            },
+            title: {
+                text: 'Live random data'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 150
+            },
+            yAxis: {
+                title: {
+                    text: 'Value'
+                },
+                plotLines: [{
+                    value: 1,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+           plotOptions: {
+              areaspline: {
+                fillOpacity: 0.5
+            }
+           },
+            legend: {
+                enabled: false
+            },
+            exporting: {
+                enabled: false
+            },
+            series: [{
+                name: 'Random data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: $scope.current
+                        });
+                    }
+                    return data;
+                }())
+            }]
+        });
+    }
 });
