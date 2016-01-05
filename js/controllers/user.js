e95a084cb9241c5bd6d62a40a82c3f21edff40fb
Thibaut <thibaut.arnoud@gmail.com>
2016-01-05 15:57:35 +0100

    update controller user.js

--- js/controllers/user.js
+++ js/controllers/user.js
@@ -1,31 +1,9 @@
-app.controller('userCtrl',function($scope,$http,$stateParams,$location)
+app.controller('userCtrl',function($scope,$http)
 {;
     $http.get("")   //appel api steam
     .success(function(r)
     {
     	console.log(r);
-        $scope.user = r.users[0];
-    });
-
-    // * check if edit request */
-    var c = $location.search().edit;
-    if(c != undefined)
-    {
-    	$http.get(apiIndex+"/me")
-	    .success(function(r)
-	    {
-	    	var lId = r.me[0].id;
-	        if(lId == $scope.user.id)
-	        {
-	        	$scope.userProfilView = "views/user/edit.html";
-	        }
-	    });
-    }
-
-    /* button tab active effect */
-    $(document).on("click",".user-navbar .btn",function()
-    {
-        $(".user-navbar .btn").removeClass("active");
-        $(this).addClass("active");
+        $scope.user = r;
     });
 });
