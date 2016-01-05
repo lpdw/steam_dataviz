8f37af390690aeaec3a640995ad30cbd765df883
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-05 12:48:09 +0100

    Référencement des api amélioré

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
-var steamDbApiIndex = "https://ravenholm.steamdb.info";
+var api = {
+  "steamDB" : "https://ravenholm.steamdb.info",
+  "steamDBGlobal" : "https://ravenholm.steamdb.info/ispywithmylittleeye",
+  "steamSpy" : "proxy.php?url=http://steamspy.com/api.php",
+  "steam" : "http://api.steampowered.com",
+  "steamDL": "http://cdn.akamai.steamstatic.com/steam/publicstats/download_traffic_per_country.json",
+}
 
 var app = angular.module('dataviz', [
-
-
+  
 ]);
 
 app.controller('mainCtrl', ['$scope', function($scope)
--- js/controllers/index.js
+++ js/controllers/index.js
@@ -1,12 +1,17 @@
 app.controller('indexCtrl',function($scope,$http)
 {
-    $http.get(steamDbApiIndex+"/753w.json")   //appel api steam
+    $http.get(api.steamDB+"/753w.json")   //appel api steam
     .success(function(r)
     {
-        console.log(r.players);
         $scope.current = r.players[r.players.length-1];
         $scope.last = r.players[r.players.length-2];
-        console.log($scope.last);
+    });
+
+    $http.get(api.steamSpy+"?request=top100in2weeks")
+    .success(function(r)
+    {
+    	$scope.top100 = _.sortBy(r, 'players_2weeks').reverse();
+    	console.log($scope.top100);
     });
     //Ici on récupère le nombre d'utilisateurs en ligne en live et on simule un changement en temps réèl (alors qu'en fait non mais les gens sont con ils verront rien mdr)
 });
