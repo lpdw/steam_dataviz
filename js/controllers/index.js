0ce6ba96873b10f2ae6872136a1fbec78fccdfe2
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-05 14:59:15 +0100

    Maj du controlleur index

--- js/controllers/index.js
+++ js/controllers/index.js
@@ -11,7 +11,23 @@ app.controller('indexCtrl',function($scope,$http)
     .success(function(r)
     {
     	$scope.top100 = _.sortBy(r, 'players_2weeks').reverse();
-    	console.log($scope.top100);
+    	$scope.topGames = $scope.top100.slice(0, 15);
+
+    	for (var i = 0; i < $scope.topGames.length; i++)
+    	{
+    		retreiveCurrentPlayers(i);	
+    	};
+
+    	function retreiveCurrentPlayers(i)
+    	{
+    		$http.get(api.steamDB+"/"+$scope.topGames[i].appid+"w.json")
+    		.success(function(t)
+    		{
+    			$scope.topGames[i].currentPlayers = t.players[t.players.length-1];
+    		});
+    	}
+
+    	console.log($scope.topGames);
     });
     //Ici on récupère le nombre d'utilisateurs en ligne en live et on simule un changement en temps réèl (alors qu'en fait non mais les gens sont con ils verront rien mdr)
 });
