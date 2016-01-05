8db1e8b8b9c7110eacb439377a7c902738af1cf9
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-05 10:14:59 +0100

    L'index est désormais un fichier PHP Cela permet d'inclure automatiquement les dépendances présentesdans les sous rep sans les ajouter à la main à chaque fois.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html ng-app="dataviz">
+<head>
+
+	<title>Steam Dataviz</title>
+	<meta charset="utf-8">
+
+	<style>
+	<?php
+	// loading all style
+	foreach (glob("css/*.css") as $filename)
+	{
+	    echo file_get_contents($filename);
+	}
+	?>
+	</style>
+
+	<!-- loading all prerequisite components -->
+	<script src="js/vendor/jquery.min.js"></script>
+	<script src="js/vendor/angular.min.js"></script>
+
+	<script>
+	<?php
+
+	// loading basic js
+	foreach (glob("js/*.js") as $filename)
+	{
+	    echo file_get_contents($filename);
+	}
+
+	// loading angular controllers
+	foreach (glob("js/controllers/*.js") as $filename)
+	{
+	    echo file_get_contents($filename);
+	}
+	?>
+	</script>
+
+</head>
+<body ng-controller="mainCtrl">
+</body>
+</html>
