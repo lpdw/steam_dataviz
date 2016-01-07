4b71dd7ee13d8ca94090121c9e2226a30d791bb9
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2016-01-07 10:13:49 +0100

    Added Game controller

--- assets/js/controllers/game.js
+++ assets/js/controllers/game.js
@@ -1,13 +1,21 @@
-app.controller('gameCtrl',function($scope,$http)
+app.controller('gameCtrl',function($scope,$http,$stateParams)
 {
-    $scope.game = {};
-    $scope.setGame = function(appid)
+    $scope.game;
+    $scope.appid = "76561197960287930";    //Gabe Newell id by default
+    if($stateParams.appid)            //if id is defined
     {
-        $http.get(api.steamDB+"/"+appid+"w.json")   //appel api steam
-        .success(function(r)
-        {
-            $scope.game = r;
-        });
+        $scope.appid = $stateParams.appid;
     }
+
+    var friendProfil;
+    var friendsOfFriend;
+
+    $http.get(api.steamSpy+"?request=appdetails&appid="+$scope.appid)   //appel api steam
+    .success(function(r)
+    {
+        console.log(api.steamSpy+"?request=appdetails&appid="+$scope.appid);
+        $scope.game = r;
+        console.log($scope.game);
+    });
     //Ici on récupère le nombre d'utilisateurs en ligne en live et on simule un changement en temps réèl (alors qu'en fait non mais les gens sont con ils verront rien mdr)
 });
